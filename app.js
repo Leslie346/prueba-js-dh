055039bb5335ffb63c745c7839d6f218658fa552
Leslie346 <lesliecruz346@gmail.com>
2021-05-20 22:37:40 -0500

    Examen JS DH

--- app.js
+++ app.js
@@ -6,17 +6,15 @@
            * pendiente
          )
 */
+{
+const tareas = require('./tareas')
 
-exports.leerTareas = function () {
+export function leerTareas() {
     /*
         En esta función debes leer y retornar las tareas registradas.
     */
-}
-
-exports.agregarTarea = function (tarea) {
-    /*
-        Registra y guarda una nueva tarea.
-    */
+        tareas.leer();
+   return 
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
@@ -24,4 +22,19 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
+   tareas.leer() 
+   let estado = process.argv[2];
+   let tempData = [];
+   switch(tareas.titulo === "terminada"){
+       case 'listar':
+       for(let i = 0; i < listado.length; i++){
+           console.log('titulo: ${listado[i].titulo status: $(listado[i].status)}')
+           tempData.push(tareas.titulo);
+           tempData.push(tareas.status);
+       }
+       break;
+   }  
 }
+}
+
+module.exports = app.js
